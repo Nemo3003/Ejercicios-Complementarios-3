46763eda680a6361cbd433647c8db69bf308f12a
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2021-11-20 00:12:51 -0300

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
-
 public class Ejercicio1 {
     public static void main(String[] args) {
         List<String> palabras = new ArrayList<>();
