dd60a9fd3a1a93571f75bf6ee8fb59770b14a4b1
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2021-11-20 00:12:35 -0300

    Update Ejercicio3.java

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,5 +1,4 @@
 import java.util.List;
-
 public class Ejercicio3 {
     public static void main(String[] args) {
         List<String> palabras = List.of("Batman", "Aquaman", "Flash", "Batgirl", "Wonder Woman", "brainiac");
